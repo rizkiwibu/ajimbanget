15fb22925270051e1110d6e088edfee2862a2e1c
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-06-08 21:10:55 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482', '0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
+global.ownerNumber = ['6283817161861', '0']
+global.ownerName = 'Rizkikun'
+global.packname = 'ikybot'
+global.author = 'ikyBot 2022'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
